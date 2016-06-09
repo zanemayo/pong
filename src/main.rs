4da5d043266dda872eca603f51babc2ee6b008ba
Zane Mayo <zanemayo@gmail.com>
2016-06-09 08:57:59 +0100

    Animation on the GPU by passing a uniform to a shader

--- src/main.rs
+++ src/main.rs
@@ -12,49 +12,54 @@ fn main() {
     use glium::{DisplayBuild, Surface};
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        uniform float t;
+
+        void main() {
+            vec2 pos = position;
+            pos.x += t;
+            gl_Position = vec4(pos, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [0.0, 0.5] };
+    let vertex3 = Vertex { position: [0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    
     let mut t: f32 = -0.5;
     loop {
 
-      t += 0.001;
+        t += 0.001;
         if t > 0.5 {
             t = -0.5;
         }
 
-
-        let vertex1 = Vertex { position: [-0.5 + t, -0.5] };
-        let vertex2 = Vertex { position: [0.0 + t, 0.5] };
-        let vertex3 = Vertex { position: [0.5 + t, -0.25] };
-        let shape = vec![vertex1, vertex2, vertex3];
-
-        let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
         let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
-        let vertex_shader_src = r#"
-            #version 140
-
-            in vec2 position;
-
-            void main() {
-                gl_Position = vec4(position, 0.0, 1.0);
-            }
-        "#;
-
-        let fragment_shader_src = r#"
-            #version 140
-
-            out vec4 color;
-
-            void main() {
-                color = vec4(1.0, 0.0, 0.0, 1.0);
-            }
-        "#;
 
         let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 1.0, 1.0);
 
-        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+        target.draw(&vertex_buffer, &indices, &program, &uniform! {t: t},  &Default::default()).unwrap();
 
         target.finish().unwrap();
 
